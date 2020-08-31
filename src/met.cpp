8b05618b9fe988e315dfb6bfc12fc58bea528e60
Christian Herwig <therwig@users.noreply.github.com>
2020-08-31 15:35:40 -0500

    cleanup word writing utils

--- src/met.cpp
+++ src/met.cpp
@@ -10,48 +10,32 @@
 #endif
 
 // pt, phi are integers
-//void met_hw(word_t inputs[NPART],  pt2_t& res_pt2, phi_t& res_phi){
-//void met_hw(word_t inputs[NPART],  word_t &output){
 void met_hw(hls::stream<PFInputWords> &inputs, hls::stream<word_t> &output){
 //#pragma HLS dataflow
 #pragma HLS pipeline ii=36
     //#pragma HLS ARRAY_PARTITION variable=inputs complete
 
     if(DEBUG) std::cout << "  HW Begin" << std::endl;
-
-    var_t temp_pt[NPART]; var_t temp_phi[NPART];  //var_t is ap_int<16>
     pt_t data_pt[NPART]; phi_t data_phi[NPART];  //var_t is ap_int<16>
-    size_t Length;
 
     PFInputWords input_array = inputs.read();
     
     for(int i = 0; i < NPART; i++){
         data_pt[i]  = input_array.data[i](63,48);
         data_phi[i] = input_array.data[i](47,32);
-        // data_pt[i]  = inputs[i](63,48);
-        // data_phi[i] = inputs[i](47,32);
-        //temp_pt[i]  = inputs[i](63,48);
-        //temp_phi[i] = inputs[i](47,32);
-
-        //data_pt[i] = temp_pt[i];
-        //data_phi[i] = temp_phi[i];
-
         if(DEBUG) std::cout<<"word pt phi: "<<data_pt[i]<<" "<<data_phi[i]<<std::endl;
-
-        //if(data_pt[i]==0 && data_phi[i]==0) continue;
     }
 #pragma HLS ARRAY_PARTITION variable=data_pt complete
 #pragma HLS ARRAY_PARTITION variable=data_phi complete
 
     // calc signed components first
-    pxy_t met_x = 0;
-    pxy_t met_y = 0;
     pxy_t sum_x = 0;
     pxy_t sum_y = 0;
 
-
  LOOP_COMPONENTS: for(int i=0; i<NPART;i++){
         // Get x, y components
+        pxy_t met_x = 0;
+        pxy_t met_y = 0;
         ProjX(data_pt[i], data_phi[i], met_x);
         ProjY(data_pt[i], data_phi[i], met_y);
         // Add to x, y sums
@@ -72,20 +56,12 @@ void met_hw(hls::stream<PFInputWords> &inputs, hls::stream<word_t> &output){
 
     res_pt = hls::sqrt(res_pt2);
 
-    //if( res_phi < 0) res_phi = (1<<16)+res_phi;
     if(DEBUG) std::cout<<"hw out res_pt&phi : "<<res_pt<<", "<<res_phi<<std::endl;
-    ap_uint<16> var_pt, var_phi;
-    ap_uint<64> var2_pt, var2_phi;
-    var_pt = res_pt;
-    var_phi = res_phi; //if( res_phi < 0 ) var_phi = (1<<16)+res_phi;
-    if(DEBUG) std::cout<<"var_pt & phi : "<<var_pt<<", "<<var_phi<<std::endl;
-    // pack outputs into a single word
-    var2_phi = var_phi;
-    var2_pt = var_pt;
-    if(DEBUG) std::cout<<"var2_pt & phi : "<<var2_pt<<", "<<var2_phi<<std::endl;
-    output.write( word_t(0 + (0<<16) + (var2_phi<<32) + (var2_pt<<48)) );
-    //output = 0 + (0<<16) + (var2_phi<<32) + (var2_pt<<48);
-    //output = 0 + (0<<16) + (res_phi<<32) + (res_pt<<48);
+
+    word_t outword = ( res_pt, res_phi, ap_uint<32>(0) );
+    output.write( outword );
+    return;
+
 
     return;
 }
